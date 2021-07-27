4e7227ab942da1a436f3aee12f5cad3e204107ce
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2021-07-27 18:51:25 +0300

    add actors and movies data

---
+++ app/src/main/java/com/myaxa/academycourse/data/models/Actor.kt
@@ -0,0 +1,6 @@
+package com.myaxa.academycourse.data.models
+
+data class Actor(
+    val name: String,
+    val avatar: String
+)
---
+++ app/src/main/java/com/myaxa/academycourse/domain/MoviesDataSource.kt
@@ -0,0 +1,75 @@
+package com.myaxa.academycourse.domain
+
+import com.myaxa.academycourse.data.models.Actor
+import com.myaxa.academycourse.data.models.Movie
+
+class MoviesDataSource {
+    fun getMovies(): List<Movie> {
+        return listOf<Movie>(
+                Movie("Mstuni", "Prikoldes",
+                        "https://clck.ru/WStsK",
+                        listOf(
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                        )
+                ),
+                Movie("Tenet", "Prikoldes", "https://clck.ru/WSuNZ",
+                        listOf(
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                        )
+                ),
+                Movie("Black Widow", "Prikoldes", "https://clck.ru/WSuBW",
+                        listOf(
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                        )
+                ),
+                Movie("Wonder Waflya 1984", "Prikoldes", "https://clck.ru/WSuZn",
+                        listOf(
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                        )
+                ),
+                Movie("Third movie", "Prikoldes", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg",
+                        listOf(
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                        )
+                ),
+                Movie("Third movie", "Prikoldes", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg",
+                        listOf(
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                        )
+                ),
+                Movie("Third movie", "Prikoldes", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg",
+                        listOf(
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                                Actor("Robert Downew J", "https://image.ibb.co/buLLjy/Ryan_Gosling.jpg"),
+                        )
+                ),
+        )
+    }
+}
