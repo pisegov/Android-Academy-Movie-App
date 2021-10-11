f1515aa3275522a02e53241e1314489086a53892
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2021-10-11 19:54:27 +0300

    feature (movies list) add movie data display

--- app/src/main/java/com/myaxa/academycourse/movies/MoviesAdapter.kt
+++ app/src/main/java/com/myaxa/academycourse/movies/MoviesAdapter.kt
@@ -47,6 +47,17 @@ abstract class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView
 class MoviesViewHolder(itemView: View) : DataViewHolder(itemView) {
     private val image: ImageView = itemView.findViewById<ImageView>(R.id.iv_movie_card_image)
     private val title: TextView = itemView.findViewById<TextView>(R.id.title_in_list)
+    private val pg: TextView = itemView.findViewById(R.id.tv_pg_movie_card)
+    private val genres: TextView = itemView.findViewById(R.id.tv_genres_movie_card)
+    private val reviewsNumber: TextView = itemView.findViewById(R.id.tv_reviews_movie_card)
+    private val length: TextView = itemView.findViewById(R.id.tv_length_movie_card)
+    private val starsList: List<ImageView> = listOf(
+        itemView.findViewById(R.id.star_list_1),
+        itemView.findViewById(R.id.star_list_2),
+        itemView.findViewById(R.id.star_list_3),
+        itemView.findViewById(R.id.star_list_4),
+        itemView.findViewById(R.id.star_list_5),
+    )
 
     fun onBind(movie: Movie) {
         Glide.with(context)
@@ -55,8 +66,25 @@ class MoviesViewHolder(itemView: View) : DataViewHolder(itemView) {
             .into(image)
 
         title.text = movie.title
-    }
 
+        pg.text = "${movie.pgAge}+"
+
+        reviewsNumber.text = movie.reviewCount.toString() + " Reviews"
+
+        length.text = "${movie.runningTime} MIN"
+
+        var genresString: String = ""
+        movie.genres.forEach { genre ->
+            genresString += "${genre.name}, "
+        }
+        genres.text = genresString.removeSuffix(", ")
+
+        starsList.forEachIndexed { index, star ->
+            val colorId = if (movie.rating > index) R.color.pink else R.color.dark_grey
+            star.setColorFilter(context.getColor(colorId))
+        }
+
+    }
 
     companion object {
         private val imageOption = RequestOptions()
